7378371057e8ee802fe6dd5f19aceb088cd0645c
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-03-27 21:34:14 +0700

    add help

--- test/createfile.c
+++ test/createfile.c
@@ -2,26 +2,9 @@
 #include "syscall.h"
 #include "copyright.h"
 
-#define maxlen 32
-
 int main()
 {
-    // int len;
-    // char filename[maxlen+1];
-
-    // // create file
-    // if(Create("text.txt")==-1)
-    // {
-    //     // printf("Error create file");
-    // } else
-    // {
-    //     // printf("Success create file");
-    // }
-    // Create("text.txt");
-
-    // char s[1024] = "abcdes";
-    // ReadString(s, 3);
-    PrintString("abcdes");
-    
+    Create("text.txt");
+ 
     Halt();
 }
